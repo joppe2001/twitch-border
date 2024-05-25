2fcaeb2656d7b898ce5db908c7d3305f18b5ecd9
joppe montezinos <34985258+joppe2001@users.noreply.github.com>
2024-05-25 20:02:04 +0200

    Update particles.js

--- public/particles.js
+++ public/particles.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const particleContainer = document.getElementById('particles');
     const numParticles = 100;
+    const numShootingStars = 5;
     
     for (let i = 0; i < numParticles; i++) {
         const particle = document.createElement('div');
@@ -10,9 +11,18 @@ document.addEventListener('DOMContentLoaded', function () {
         particle.style.height = `${size}px`;
         particle.style.top = `${Math.random() * 100}%`;
         particle.style.left = `${Math.random() * 100}%`;
-        particle.style.animationDelay = `${Math.random() * 3}s`;
-        particle.style.animationDuration = `${Math.random() * 10 + 3}s`;
+        particle.style.animationDelay = `${Math.random() * 5}s`;
+        particle.style.animationDuration = `${Math.random() * 10 + 5}s`;
         particleContainer.appendChild(particle);
+    }
+    
+    for (let i = 0; i < numShootingStars; i++) {
+        const shootingStar = document.createElement('div');
+        shootingStar.className = 'shooting-star';
+        shootingStar.style.top = `${Math.random() * 100}%`;
+        shootingStar.style.left = `${Math.random() * 100}%`;
+        shootingStar.style.animationDelay = `${Math.random() * 10}s`;
+        particleContainer.appendChild(shootingStar);
     }
 });
 
@@ -24,7 +34,7 @@ style.innerHTML = `
     background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,0.5) 100%);
     border-radius: 50%;
     box-shadow: 0 0 5px rgba(255, 255, 255, 0.7), 0 0 10px rgba(255, 255, 255, 0.5);
-    animation: particle-move 2s linear infinite;
+    animation: particle-move 10s linear infinite;
 }
 
 @keyframes particle-move {
@@ -34,6 +44,27 @@ style.innerHTML = `
     }
     100% {
         transform: translateY(-150%) translateX(100%);
+        opacity: 0;
+    }
+}
+
+.shooting-star {
+    position: absolute;
+    width: 2px;
+    height: 100px;
+    background: linear-gradient(to bottom, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0));
+    border-radius: 50%;
+    box-shadow: 0 0 20px rgba(255, 255, 255, 0.7), 0 0 40px rgba(255, 255, 255, 0.5);
+    animation: shooting-star-move 3s linear infinite;
+}
+
+@keyframes shooting-star-move {
+    0% {
+        transform: translateY(-100vh) translateX(-100vw);
+        opacity: 1;
+    }
+    100% {
+        transform: translateY(100vh) translateX(100vw);
         opacity: 0;
     }
 }
